216c4a696247f37a92afb62aad0122396697194f
Robert Adams <misterblue@misterblue.com>
2022-02-06 20:57:24 +0000

    First update to image-herbal3d. Doesn't work yet.

--- image-opensim-herbal3d/config/scripts/initializeDb.sh
+++ image-opensim-herbal3d/config/scripts/initializeDb.sh
@@ -2,25 +2,36 @@
 # Script to create database 'opensim' in mysql if it does not exist.
 # Someone has set the environment variables before running this.
 # Needs:
+#       MYSQL_DB_SOURCE
 #       MYSQL_ROOT_PASSWORD
 #       OPENSIM_DB_PASSWORD
 
 OPENSIMBIN=${OPENSIMBIN:-/home/opensim/opensim/bin}
 OPENSIMCONFIG=${OPENSIMCONFIG:-$OPENSIMBIN/config}
 
+echo "opensim-docker: initializeDb.sh: "
+
 cd "$OPENSIMCONFIG"
 
 if [[ ! -z "$MYSQL_ROOT_PASSWORD" ]] ; then
-    SQLCMDS=${OPENSIMCONFIG}/mymy$$
-    SQLOPTIONS=${OPENSIMCONFIG}/mymyoptions$$
+    for needed in "MYSQL_DB_DB" "MYSQL_DB_SOURCE" "MYSQL_DB_USER" "MYSQL_DB_USER_PASSWORD" ; do
+        if [[ -z "$!needed" ]] ; then
+            echo "opensim-docker: initializeDb.sh: missing required env parameter $needed"
+            echo "opensim-docker: initializeDb.sh: DATABASE NOT INITIALIZE"
+            exit 5
+        fi
+    done
+
+    SQLCMDS=/tmp/mymy$$
+    SQLOPTIONS=/tmp/mymyoptions$$
     rm -f "$SQLCMDS"
     rm -f "$SQLOPTIONS"
 
     cat > "$SQLCMDS" <<EOFFFF
-CREATE DATABASE opensim;
-USE opensim;
-CREATE USER 'opensim'@'%' IDENTIFIED BY '$MYSQL_DB_PASSWORD';
-GRANT ALL ON opensim.* to 'opensim'@'%';
+CREATE DATABASE $MYSQL_DB_DB;
+USE $MYSQL_DB_DB;
+CREATE USER '$MYSQL_DB_USER'@'%' IDENTIFIED BY '$MYSQL_DB_USER_PASSWORD';
+GRANT ALL ON $MYSQL_DB_DB.* to '$MYSQL_DB_USER'@'%';
 quit
 EOFFFF
     cat > "$SQLOPTIONS" <<EOFFFF
@@ -32,23 +43,24 @@ EOFFFF
 
     # If the database is started at the same time, what for it to initialize
     until mysql --defaults-extra-file=$SQLOPTIONS -e "show databases" ; do
-        echo "initializeDb.sh: Waiting on database to be ready"
+        echo "opensim-docker: initializeDb.sh: Waiting on database to be ready"
         sleep 2
     done
-    echo "initializeDb.sh: Database is ready"
+    echo "opensim-docker: initializeDb.sh: Database is ready"
+
 
     HASDB=$(mysql --defaults-extra-file=$SQLOPTIONS -e "show databases" | grep "^opensim$")
 
     if [[ -z "$HASDB" ]] ; then
-        echo "creating opensim database"
+        echo "opensim-docker: initialzeDb.sh: creating opensim database"
         mysql --defaults-extra-file=$SQLOPTIONS < "$SQLCMDS"
     else
-        echo "opensim database has already been created"
+        echo "opensim-docker: initialzeDb.sh: opensim database has already been created"
     fi
 
     rm -f "$SQLCMDS"
     rm -f "$SQLOPTIONS"
 else
-    echo "Not configuring MYSQL"
+    echo "opensim-docker: initializeDb.sh: Not configuring SQL database"
 fi
 
--- image-opensim-herbal3d/run-herbal3d.sh
+++ image-opensim-herbal3d/run-herbal3d.sh
@@ -6,17 +6,19 @@
 #    running things.
 
 # Be sure to set environment variables:
-#    CONFIGKEY=passwordForCryptFile
-#    CONFIG_NAME=nameOfRunConfiguration
+#    CONFIGKEY=passwordForCryptFile (optional. Not needed if os-secrets is not ccrypt'ed)
+#    CONFIG_NAME=nameOfRunConfiguration (default 'standalone' of not supplied)
 #    EXTERNAL_HOSTNAME=IPorDNSnameForSimulator
+
+# If this environment variable is defined, the configuration files are updated with
+#    configuration values in the container (at container runtime). Otherwise, the
+#    configuration files are updated with values external to the container (in the
+#    filesystem of the invoker and the internal directory 'bin/config/ is mounted
+#    as the local 'config' directory.
+# export OS_DOCKER_CONTAINER_CONFIG="yes"
 
 BASE=$(pwd)
 
-if [[ -z "$CONFIGKEY" ]] ; then
-    echo "Configuration password environment variable CONFIGKEY is not set."
-    echo "NOT STARTING!"
-    exit 3
-fi
 if [[ -z "$EXTERNAL_HOSTNAME" ]] ; then
     echo "Environment variable EXTERNAL_HOSTNAME is not set."
     echo "NOT STARTING!"
@@ -30,13 +32,32 @@ export CONFIG_NAME=${CONFIG_NAME:-standalone}
 # This export fakes out the environment setup script to look for files in
 #    build environment rather than in run environment.
 export OPENSIMBIN=$BASE
-source config/scripts/setEnvironment.sh
-unset OPENSIMBIN
 
+# if configuration files are external to the container, run the configuration
+if [[ -z "$OS_DOCKER_CONTAINER_CONFIG" ]] ; then
+    echo "opensim-docker: running configuration file initialization"
+    config/scripts/updateConfigFiles.sh
+    config/scripts/linkInConfigs.sh
+else
+    echo "opensim-docker: delaying configuration file initialization to configuration start"
+    # Just set the configuration variables into the environment
+    source config/scripts/setEnvironment.sh
+fi
+
+# Use the generic docker-compose file or the one specific to the configuration if it exists
 cd "$BASE"
-COMPOSEFILE=./docker-compose.yml
-if [[ -e "config/$CONFIG_NAME/docker-compose.yml" ]] ; then
-    COMPOSEFILE="config/$CONFIG_NAME/docker-compose.yml"
+COMPOSEFILE="config/config-${CONFIG_NAME}/docker-compose.yml"
+if [[ -z "$OS_DOCKER_CONTAINER_CONFIG" ]] ; then
+    # if using external configuration, include docker-compose with the mount
+    COMPOSEFILE="config/config-${CONFIG_NAME}/docker-compose-external-config.yml"
+fi
+
+# Local directory for storage of sql persistant data (so region
+#    contents persists between container restarts).
+# This must be the same directory as in config-$CONFIG_NAME/docker-compose.yml.
+if [[ ! -d "$HOME/opensim-sql-data" ]] ; then
+    mkdir -p "$HOME/opensim-sql-data"
+    chmod o+w "$HOME/opensim-sql-data"
 fi
 
 docker-compose \
@@ -44,5 +65,4 @@ docker-compose \
     --project-name opensim-herbal3d \
     --project-directory "$BASE" \
     up \
-    -d \
-    opensim basiljs
+    --detach
