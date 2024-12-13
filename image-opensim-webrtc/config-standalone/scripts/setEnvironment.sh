83f7f5f7a26b7c83c70ec463087c150ed098bb3d
Robert Adams <misterblue@misterblue.com>
2024-12-13 01:52:41 +0000

    Add image-opensim-webrtc which is for building an OpenSimulator Docker image with os-webrtc-janus included.

---
+++ image-opensim-webrtc/config-standalone/scripts/setEnvironment.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+# Script that sets up the environment variables
+# This script is run before the docker-compose file is run to get the
+#     database password and it is run when the opensim Docker container starts
+#     to get all the values for the configuration files.
+
+OPENSIMBIN=${OPENSIMBIN:-/home/opensim/opensim/bin}
+OPENSIMCONFIG=${OPENSIMCONFIG:-$OPENSIMBIN/config}
+
+cd "$OPENSIMCONFIG"
+
+# See if we have encrypted secrets
+unset HAVE_SECRETS
+if [[ ! -z "$OS_CONFIGKEY" ]] ; then
+    for secretsFile in $OPENSIMCONFIG/os-secrets.crypt ; do
+        if [[ -e "$secretsFile" ]] ; then
+            echo "opensim-docker: setEnvironment.sh: have secrets file \"{$secretsFile}\""
+            source <(ccrypt -c -E OS_CONFIGKEY "$secretsFile")
+            HAVE_SECRETS=yes
+            break;
+        else
+            echo "opensim-docker: setEnvironment.sh: no encrypted secrets file"
+        fi
+    done
+fi
+
+# If no encrypted secrets, maybe unsecure plain-text secrets are available
+if [[ -z "$HAVE_SECRETS" ]] ; then
+    echo "opensim-docker: setEnvironment.sh: trying plain text secrets"
+    for secretsFile in $OPENSIMCONFIG/os-secrets ; do
+        if [[ -e "$secretsFile" ]] ; then
+            echo "opensim-docker: setEnvironment.sh: plain text secrets from \"${secretsFile}\""
+            source "$secretsFile"
+            break;
+        else
+            echo "opensim-docker: setEnvironment.sh: no plain text secrets file"
+        fi
+    done
+fi
---
+++ image-opensim-webrtc/restart-opensim.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Stop the running opensimulator
+
+BASE=$(pwd)
+
+# Get the container parameters into the environment
+# (Needs to be in the environment for the docker-compose.yml file)
+source ./envToEnvironment.sh
+
+export OS_CONFIG=${OS_CONFIG:-standalone}
+
+echo "Restarting configuration $CONFIG_NAME from docker-compose.yml"
+
+docker-compose \
+    --file docker-compose.yml \
+    --env-file ./env \
+    --project-name opensim-${OS_CONFIG} \
+    restart
